fd9000efaae09f051d692b4bc9c9dfc984ee72ec
Aglarov <shamil.aglarov@gmail.com>
2022-05-13 02:22:34 +0300

    12.05.2022 - created 2 struct User and Order, and created 3 init where one init optional

--- preparation for classes/main.swift
+++ preparation for classes/main.swift
@@ -7,4 +7,55 @@
 
 import Foundation
 
+struct User {
+    let name: String
+    let age: Int
+}
 
+///передаем цену и скидку
+struct Order {
+    let id: String
+    let price: Int
+    let user: User
+    init(
+        id: String,
+         price: Int,
+         discountedPersent: Int,
+         user: User
+        )
+    {
+        self.id = id
+        self.price = (price * discountedPersent) / 100
+        self.user = user
+    }
+    
+    ///передаем цену с учетом скидки
+    init(id: String,
+         discountedPrice: Int,
+         user: User
+        )
+    {
+        self.id = id
+        self.price = discountedPrice
+        self.user = user
+    }
+    
+    init?(id: String?,
+          discountedPrice: Int,
+          user: User
+        )
+    {
+        guard let id = id else { return nil }
+        
+        self.init(id: id,
+                  discountedPrice: discountedPrice,
+                  user: user
+                 )
+    }
+}
+
+let user = User(name: "Shamil", age: 33)
+
+let anotherOrder = Order(id: nil, discountedPrice: 0, user: user)
+
+print(anotherOrder ?? "it not created")
