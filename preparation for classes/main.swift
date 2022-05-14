504afa6584de324d9474a5f812e4ab43257df66d
Aglarov <shamil.aglarov@gmail.com>
2022-05-14 15:39:14 +0300

    14.05.2022 - created func printMenu where writed 3 value

--- preparation for classes/main.swift
+++ preparation for classes/main.swift
@@ -7,4 +7,26 @@
 
 import Foundation
 
+func printMenu() {
+    print("""
+          ИГРА БОЙЦОВСКИЙ КЛУБ
+          
+          1 - начать игру
+          2 - правила
+          3 - выход
+          """)
+    
+    let option = readLine() ?? "nil"
+    
+    switch option {
+    case "1":
+        print("Старт Игры ")
+    case "2":
+        print("ТУТ ДОЛЖНЫ БЫТЬ ПРАВИЛА ИГРЫ")
+    case "3": print("Вы вышли из игры!")
+    default:
+        printMenu()
+    }
+}
 
+printMenu()
