7cf58f6441cf87bb3471bc3d79e7a95975192fa4
Aglarov <shamil.aglarov@gmail.com>
2022-06-23 21:51:51 +0300

    Theme Generic Example-1

--- preparation for classes/main.swift
+++ preparation for classes/main.swift
@@ -7,4 +7,25 @@
 
 import Foundation
 
+//структура сохраняет словарь из ключей и их значений
 
+struct Storage<T> {
+    var elements: [String : T] = [:]
+    
+    mutating func setElement(_ key: T, for element: String) {
+        elements[element] = key
+    }
+    mutating func getElement(for key: String) -> T? {
+        return elements[key]
+    }
+}
+
+var storage = Storage<Int>()
+
+storage.setElement(1, for: "Книга Усова по Swift")
+storage.setElement(2, for: "Книга Усова по C#")
+storage.setElement(3, for: "Книга Усова по Piton")
+
+let printElement = storage.getElement(for: "Книга Усова по C#")
+
+print(printElement)
